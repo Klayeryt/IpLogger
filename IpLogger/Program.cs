3f9e4de36aa4347a7fc0d978b8cb717c66aba198
Klayeryt <90210691+Klayeryt@users.noreply.github.com>
2022-07-21 22:24:32 +0700

    Добавьте файлы проекта.

---
+++ IpLogger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using MySql.Data.MySqlClient;
+        String host = System.Net.Dns.GetHostName();
+        Console.WriteLine("Имя ПК: " + host);
+        Console.WriteLine("Список доступных IP: ");
+        for (int z = 0; z < 4; z++)
+        {
+            System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[z];
+            Console.WriteLine(ip);
+        };
+        Console.ReadLine();
